6f9181edeceea0fbbb499ce9147ecda91534b8f1
yanyiyang2000 <szyyy2000@gmail.com>
2024-07-18 21:09:21 -0400

    update

---
+++ include/tags.h
@@ -0,0 +1,109 @@
+#ifndef TAG_H
+#define TAG_H
+
+#include <stdint.h>
+
+
+struct Tag {
+    char     *Name;   /* The tag name */
+    uint16_t Number;  /* The tag number in hexadecimal */
+};
+
+static struct Tag tags[] = {
+    /* Baseline Tags [TIFF Rev 6.0, pp.117-118] */
+    {"Image Width",                     0x0100},    // The image width
+    {"Image Height",                    0x0101},    // The image height
+    {"Bits Per Sample",                 0x0102},    // The number of bits per component
+    {"Compression",                     0x0103},    // 1 = Uncompressed. 2 = CCITT 1D. 3 = Group 3 fax. 4 = Group 4 fax. 5 = LZW. 6 = JPEG. 32773 = PackBits
+    {"Photometric Interpretation",      0x0106},    // 0 = WhiteIsZero. 1 = BlackIsZero. 2 = RGB. 3 = Palette color. 4 = Transparency mask. 5 = Seperated. 6 = YCbCr. 
+    {"Image Description",               0x010E},    // The image description
+    {"Make",                            0x010F},    // The manufacturer of the scanner, video digitizer, or other type of equipment used to generate the image
+    {"Model",                           0x0110},    // The model name or number of the scanner, video digitizer, or other type of equipment used to generate the image
+    {"Orientation",                     0x0112},    // The orientation of the image with respect to the rows and columns
+    {"Samples Per Pixel",               0x0115},    // The number of components per pixel
+    {"X Resolution",                    0x011A},    // The number of pixels per ResolutionUnit in the ImageWidth
+    {"Y Resolution",                    0x011B},    // The number of pixels per ResolutionUnit in the ImageLength
+    {"Resolution Unit",                 0x0128},    // 1 = No absolute unit of measurement. 2 = Inch. 3 = Centimeter.
+    {"Software",                        0x0131},    // The name and version number of the software package(s) used to create the image
+    {"DateTime",                        0x0132},    // The date and time of the image creation
+    {"Host Computer",                   0x013C},    // The computer and/or operating system in use at the time of image creation
+
+    /* Extension Tags [TIFF Rev 6.0, pp.117-118] */
+    {"Tile Width",                      0x0142},    // The tile width in pixels
+    {"Tile Length",                     0x0143},    // The tile length (height) in pixels
+    {"JPEG Interchange Format",         0x0201},    // The offset to JPEG SOI in bytes
+    {"JPEG Interchange Format Length",  0x0202},    // The length of JPEG data in bytes
+    {"Y Cb Cr Positioning",             0x0213},    // The positioning of subsampled chrominance components relative to luminance samples
+
+    /* Private Tags [Exif v3.0, pp.38-39] */
+    {"Exif IFD Offset",                 0x8769},    // The offset of Exif IFD from IFH in bytes
+    {"GPS Info IFD Offset",             0x8825},    // The offset of GPS Info IFD from IFH in bytes
+    {"Interoperability IFD Offset",     0xA005},    // The offset of Interoperability IFD from IFH in bytes
+
+    /* Private IFD (Exif IFD) Tags [Exif v3.0, pp.54-57] */
+    {"Exposure Time",                   0x829A},    // The exposure time in seconds
+    {"F Number",                        0x829D},    // The F number
+    {"Exposure Program",                0x8822},    // 0 = Not define. 1 = Manual. 2 = Normal program. 3 = Aperture priority. 4 = Shutter priority. 5 = Creative program. 6 = Action program. 7 = Portrait mode. 8 = Landscape mode.
+    {"Photographic Sensitivity",        0x8827},    // The sensitivity of the camera or input device. It could be standard output sensitivity (SOS), recommended exposure index (REI) or ISO speed.
+    {"Exif Version",                    0x9000},    // The Exif version (4-byte ASCII string "0300" for Exif version 3.0)
+    {"DateTime Original",               0x9003},    // The date and time when the image was generated
+    {"DateTime Digitized",              0x9004},    // The date and time when the image was stored as digital date
+    {"Offset Time",                     0x9010},    // The time difference from UTC including daylight saving time of the DateTime tag
+    {"Offset Time Original",            0x9011},    // The time difference from UTC including daylight saving time of the DateTimeOriginal tag
+    {"Offset Time Digitized",           0x9012},    // The time difference from UTC including daylight saving time of the DateTimeDigitized tag
+    {"Components Configuration",        0x9101},    // 0 = Does not exist. 1 = Y. 2 = Cb. 3 = Cr. 4 = R. 5 = G. 6 = B.
+    {"Shutter Speed Value",             0x9201},    // The shutter speed in APEX value
+    {"Aperture Value",                  0x9202},    // The lens aperture in APEX value
+    {"Brightness Value",                0x9203},    // The brightness in APEX value
+    {"Exposure Bias Value",             0x9204},    // The exposure bias in APEX value
+    {"Max Aperture Value",              0x9205},    // The max aperture value
+    {"Metering Mode",                   0x9207},    // 0 = Unknown. 1 = Average. 2 = Center-weighted average. 3 = Spot. 4 = Multi-spot. 5 = Pattern. 6 = Partial. 7 = Other.
+    {"Light Source",                    0x9208},    // The light source
+    {"Flash",                           0x9209},    // The status of flash
+    {"Focal Length",                    0x920A},    // The actual focal length in mm
+    {"Subject Area",                    0x9214},    // The location and area of the main subject in the overall scene
+    {"Maker Note",                      0x927C},    // The maker note (the offset of maker note in bytes)
+    {"User Comment",                    0x9286},    // The user comment
+    {"Subsec Time Original",            0x9291},    // The subseconds of the DateTimeOriginal tag
+    {"Subsec Time Digitized",           0x9292},    // The subseconds of the DateTimeDigitize tag
+    {"Flashpix Version",                0xA000},    // The Flashpix format version
+    {"Color Space",                     0xA001},    // 1 = sRGB. 2 = Adobe RGB. 65533 = Wide Gamut RGB. 65534 = ICC profile. 65535 = Uncalibrated
+    {"Pixel X Dimension",               0xA002},    // The width of the image
+    {"Pixel Y Dimension",               0xA003},    // The height of the image
+    {"Sensing Method",                  0xA217},    // 1 = Not defined. 2 = One-chip color area sensor. 3 = Two-chip color area sensor. 4 = Three-chip color area sensor. 5 = Color sequential area sensor. 7 = Trilinear sensor. 8 = Color sequential linear sensor.
+    {"File Source",                     0xA300},    // 1 = Scanner of transparent type. 2 = Scanner of reflex type. 3 = DSC.
+    {"Scene Type",                      0xA301},    // 1 = A directly photographed image.
+    {"Custom Rendered",                 0xA401},    // 0 = Normal process. 1 = Custom process.
+    {"Exposure Mode",                   0xA402},    // 0 = Auto exposure. 1 = Manual exposure. 2 = Auto bracket.
+    {"White Balance",                   0xA403},    // 0 = Auto white balance. 1 = Manual white balance.
+    {"Digital Zoom Ratio",              0xA404},    // The digital zoom ratio
+    {"Focal Length In 35mm Film",       0xA405},    // The equivalent focal length of a 35mm film camera
+    {"Scene Capture Type",              0xA406},    // 0 = Standard. 1 = Landscape. 2 = Portrait. 3 = Night scene.
+    {"Gain Control",                    0xA407},    // 0 = None. 1 = Low gain up. 2 = High gain up. 3 = Low gain down. 4 = High gain down.
+    {"Contrast",                        0xA408},    // 0 = Normal. 1 = Soft. 2 = Hard.
+    {"Saturation",                      0xA409},    // 0 = Normal. 1 = Low saturation. 2 = High saturation.
+    {"Sharpness",                       0xA40A},    // 0 = Normal. 1 = Soft. 2 = Hard.
+    {"Lens Specification",              0xA432},    // Value 1: minimum focal length in mm. Value 2: maximum focal length in mm. Value 3: minimum F number in the minimum focal length. Value 4: minimum F number in the maximum focal length.
+    {"Lens Make",                       0xA433},    // The manufacturer of the lens
+    {"Lens Model",                      0xA434},    // The model name or number of the lens
+    {"Composite Image",                 0xA460},    // 0 = Unknown. 1 = Not a composite image. 2 = General composite image. 3 = Composite image captured while shooting.
+
+    /* Private IFD (GPS Info IFD) Tags [Exif v3.0, pp.90-91] */
+    {"GPS Latitude Ref",                0x0001},    // N = North latitude. S = South latitude.
+    {"GPS Latitude",                    0x0002},    // The latitude
+    {"GPS Longitude Ref",               0x0003},    // E = East longitude. W = West longitude.
+    {"GPS Longitude",                   0x0004},    // The longitude
+    {"GPS Altitude Ref",                0x0005},    // 0 = Above sea level. 1 = Below se level.
+    {"GPS Altitude",                    0x0006},    // The altitude
+    {"GPS Time Stamp",                  0x0007},    // The GPS time in UTC
+    {"GPS Speed Ref",                   0x000C},    // K = Kilometers per hour. M = Miles per hour. N = knots
+    {"GPS Speed",                       0x000D},    // The speed of GPS receiver movement
+    {"GPS Img Direction Ref",           0x0010},    // T = True direction. M = Magnetic direction
+    {"GPS Img Direction",               0x0011},    // The direction of the image
+    {"GPS Dest Bearing Ref",            0x0017},    // T = True direction. M = Magnetic direction
+    {"GPS Dest Bearing",                0x0018},    // The bearing of destination
+    {"GPS Date Stamp",                  0x001D},    // The GPS date in UTC
+    {"GPS H Positioning Error",         0x001F}     // The horizontal positioning error
+};
+
+#endif /* TAG_H */
--- source/exif.c
+++ source/exif.c
@@ -8,6 +8,7 @@
 
 #include "jpeg.h"
 #include "exif.h"
+#include "tags.h"
 
 
 static bool need_byte_swap = false;
@@ -192,6 +193,23 @@ void ifd_construct(struct EXIF_Segment *seg, uint8_t idx, uint32_t ifd_ofst) {
                 }
             }
 
+            /* Construct EXIF IFD and GPS IFD if exist */
+            switch (curr_de->Tag) {
+                case 0x8769: {
+                    seg->EXIF_IFD = calloc(1, sizeof(struct Image_File_Directory));
+                    ifd_construct(seg, 1, val_ofst);
+                    break;
+                }
+
+                case 0x8825: {
+                    seg->GPS_IFD = calloc(1, sizeof(struct Image_File_Directory));
+                    ifd_construct(seg, 2, val_ofst);
+                    break;
+                }
+
+                default: break;
+            }
+
             /* Skip VALUE OFFSET, now pointing at TAG of the next DE */
             ptr += 4;
         }
@@ -214,6 +232,8 @@ void ifd_construct(struct EXIF_Segment *seg, uint8_t idx, uint32_t ifd_ofst) {
 
 void ifd_parse(struct EXIF_Segment *seg, uint8_t idx) {
     struct Image_File_Directory *curr_ifd = NULL;
+    struct Directory_Entry      *curr_de  = NULL;
+    char *tag_name = NULL;
 
     switch (idx) {
         case 0: {
@@ -235,8 +255,193 @@ void ifd_parse(struct EXIF_Segment *seg, uint8_t idx) {
     }
 
     while (curr_ifd != NULL && curr_ifd->DE_Count != 0) {
+
+        printf("┌────────────────────────────────┬───────────┬───────┬───────────────────────────────────────────────────┐\n");
+        printf("│             Tag                │   Type    │ Count │                       Value                       │\n");
+        printf("├────────────────────────────────┼───────────┼───────┼───────────────────────────────────────────────────┤\n");
+
         for (uint16_t i = 0; i < curr_ifd->DE_Count; i++) {
-            printf("Tag: 0x%04"PRIX16", Type: %2"PRIX16", Val Cnt: %2"PRId16"\n", curr_ifd->DEs[i].Tag, curr_ifd->DEs[i].Value_Type, curr_ifd->DEs[i].Value_Count);
+            curr_de = &(curr_ifd->DEs[i]);
+
+            /* Obtain TAG description */
+            for (uint16_t i = 0; i < (sizeof(tags)/sizeof(struct Tag)); i++) {
+                if (tags[i].Number == curr_de->Tag) {
+                    tag_name = tags[i].Name;
+                    break;
+                }
+            }
+
+            /* In case of unknown TAG, print the TAG in hex */
+            if (tag_name == NULL) {
+                printf("│ 0x%-25"PRIX16" │ %-9s │ %-5s │ %-49s │\n", curr_de->Tag, "", "", "");
+            }
+            
+            switch (curr_de->Value_Type) {
+                case BYTE: {
+                    uint8_t *ptr = (uint8_t *)(curr_de->Value);
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49"PRIu8" │\n", tag_name, "BYTE", curr_de->Value_Count, *ptr);
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        printf("│ %-30s | %-9s │ %-5s │ %-49"PRIu8" │\n", "", "", "", *(ptr + i));
+                    }
+                    break;
+                }
+
+                case SBYTE: {
+                    int8_t *ptr = (int8_t *)(curr_de->Value);
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49"PRId8" │\n", tag_name, "SBYTE", curr_de->Value_Count, *ptr);
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        printf("│ %-30s | %-9s │ %-5s │ %-49"PRId8" │\n", "", "", "", *(ptr + i));
+                    }
+                    break;
+                }
+
+                case ASCII: {
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49s │\n", tag_name, "ASCII", curr_de->Value_Count, (char *)(curr_de->Value));
+                    break;
+                }
+
+                case SHORT: {
+                    uint16_t *ptr = (uint16_t *)(curr_de->Value);
+                    uint16_t val  = 0;
+
+                    val = (need_byte_swap) ? __builtin_bswap16(*ptr) : *ptr;
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49"PRIu16" │\n", tag_name, "SHORT", curr_de->Value_Count, val);
+
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        val = (need_byte_swap) ? __builtin_bswap16(*ptr) : *(ptr + i);
+                        printf("│ %-30s | %-9s │ %-5s │ %-49"PRIu16" │\n", "", "", "", val);
+                    }
+
+                    break;
+                }
+
+                case SSHORT: {
+                    int16_t *ptr = (int16_t *)(curr_de->Value);
+                    int16_t val  = 0;
+
+                    val = (need_byte_swap) ? __builtin_bswap16(*ptr) : *ptr;
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49"PRId16" │\n", tag_name, "SSHORT", curr_de->Value_Count, val);
+
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        val = (need_byte_swap) ? __builtin_bswap16(*ptr) : *(ptr + i);
+                        printf("│ %-30s | %-9s │ %-5s │ %-49"PRId16" │\n", "", "", "", val);
+                    }
+
+                    break;
+                }
+
+                case LONG: {
+                    uint32_t *ptr = (uint32_t *)(curr_de->Value);
+                    uint32_t val  = 0;
+
+                    val = (need_byte_swap) ? __builtin_bswap32(*ptr) : *ptr;
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49"PRIu32" │\n", tag_name, "LONG", curr_de->Value_Count, val);
+
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        val = (need_byte_swap) ? __builtin_bswap32(*ptr) : *(ptr + i);
+                        printf("│ %-30s | %-9s │ %-5s │ %-49"PRIu32" │\n", "", "", "", val);
+                    }
+
+                    break;
+                }
+
+                case SLONG: {
+                    int32_t *ptr = (int32_t *)(curr_de->Value);
+                    int32_t val  = 0;
+
+                    val = (need_byte_swap) ? __builtin_bswap32(*ptr) : *ptr;
+                    printf("│ %-30s │ %-9s │ %-5"PRId32" │ %-49"PRId32" │\n", tag_name, "SLONG", curr_de->Value_Count, val);
+
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        val = (need_byte_swap) ? __builtin_bswap32(*ptr) : *(ptr + i);
+                        printf("│ %-30s | %-9s │ %-5s │ %-49"PRId32" │\n", "", "", "", val);
+                    }
+
+                    break;
+                }
+
+                case RATIONAL: {
+                    uint32_t *ptr = (uint32_t *)(curr_de->Value);
+                    uint32_t val1 = 0;
+                    uint32_t val2 = 0;
+
+                    val1 = (need_byte_swap) ? __builtin_bswap32(*ptr) : *ptr;
+                    val2 = (need_byte_swap) ? __builtin_bswap32(*(ptr + 1)) : *(ptr + 1);
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49f │\n", tag_name, "RATIONAL", curr_de->Value_Count, (double)(val1 / val2));
+
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        val1 = (need_byte_swap) ? __builtin_bswap32(*(ptr + i)) : *(ptr + i);
+                        val2 = (need_byte_swap) ? __builtin_bswap32(*(ptr + 2*i + 1)) : *(ptr + 2*i + 1);
+                        printf("│ %-30s | %-9s │ %-5s │ %-49f │\n", "", "", "", (double)(val1 / val2));
+                    }
+
+                    break;
+                }
+
+                case SRATIONAL: {
+                    int32_t *ptr = (int32_t *)(curr_de->Value);
+                    int32_t val1 = 0;
+                    int32_t val2 = 0;
+
+                    val1 = (need_byte_swap) ? __builtin_bswap32(*ptr) : *ptr;
+                    val2 = (need_byte_swap) ? __builtin_bswap32(*(ptr + 1)) : *(ptr + 1);
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49f │\n", tag_name, "SRATIONAL", curr_de->Value_Count, (double)(val1 / val2));
+
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        val1 = (need_byte_swap) ? __builtin_bswap32(*(ptr + i)) : *(ptr + i);
+                        val2 = (need_byte_swap) ? __builtin_bswap32(*(ptr + 2*i + 1)) : *(ptr + 2*i + 1);
+                        printf("│ %-30s | %-9s │ %-5s │ %-49f │\n", "", "", "", (double)(val1 / val2));
+                    }
+
+                    break;
+                }
+
+                case FLOAT: {
+                    float *ptr = (float *)(curr_de->Value);
+                    float val  = 0;
+
+                    val = (need_byte_swap) ? __builtin_bswap32(*ptr) : *ptr;
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49f │\n", tag_name, "FLOAT", curr_de->Value_Count, val);
+                    
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        val = (need_byte_swap) ? __builtin_bswap32(*(ptr + i)) : *(ptr + i);
+                        printf("│ %-30s | %-9s │ %-5s │ %-49f │\n", "", "", "", val);
+                    }
+
+                    break;
+                }
+
+                case DOUBLE: {
+                    double *ptr = (double *)(curr_de->Value);
+                    double val  = 0;
+
+                    val = (need_byte_swap) ? __builtin_bswap64(*ptr) : *ptr;
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49f │\n", tag_name, "DOUBLE", curr_de->Value_Count, val);
+                    
+                    for (uint16_t i = 0; i < curr_de->Value_Count - 1; i++) {
+                        val = (need_byte_swap) ? __builtin_bswap64(*(ptr + i)) : *(ptr + i);
+                        printf("│ %-30s | %-9s │ %-5s │ %-49f │\n", "", "", "", val);
+                    }
+
+                    break;
+                }
+
+                case UNDEFINED: {
+                    printf("│ %-30s │ %-9s │ %-5"PRIu32" │ %-49s │\n", tag_name, "UNDEFINED", curr_de->Value_Count, "");
+                    break;
+                }
+
+                default: {
+                    printf("Unknown VALUE TYPE\n");
+                    return;
+                }
+            }
+        
+            if (i == curr_ifd->DE_Count - 1) {
+                printf("└────────────────────────────────┴───────────┴───────┴───────────────────────────────────────────────────┘\n");
+            } else {
+                printf("├────────────────────────────────┼───────────┼───────┼───────────────────────────────────────────────────┤\n");
+            }
         }
 
         curr_ifd = curr_ifd->Next_IFD;
